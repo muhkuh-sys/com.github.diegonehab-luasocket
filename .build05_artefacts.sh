e7ea8dfeb2dbc2a3886d1459f3984ede976fe8ca
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-10-24 15:54:21 +0200

    Add container build scripts.

---
+++ .build05_artefacts.sh
@@ -0,0 +1,97 @@
+#! /bin/bash
+set -e
+
+#-----------------------------------------------------------------------------
+#
+# Build all artefacts.
+#
+pushd build
+rm -rf com.github.brimworks-lua5.1-lua-zlib
+rm -rf com.github.brimworks-lua5.2-lua-zlib
+rm -rf com.github.brimworks-lua5.3-lua-zlib
+rm -rf lua5.1
+rm -rf lua5.2
+rm -rf lua5.3
+
+mkdir com.github.brimworks-lua5.1-lua-zlib
+mkdir com.github.brimworks-lua5.2-lua-zlib
+mkdir com.github.brimworks-lua5.3-lua-zlib
+mkdir -p lua5.1/windows_x86
+mkdir -p lua5.1/windows_x86_64
+mkdir -p lua5.1/ubuntu_1404_x86
+mkdir -p lua5.1/ubuntu_1404_x86_64
+mkdir -p lua5.1/ubuntu_1604_x86
+mkdir -p lua5.1/ubuntu_1604_x86_64
+mkdir -p lua5.1/ubuntu_1704_x86
+mkdir -p lua5.1/ubuntu_1704_x86_64
+mkdir -p lua5.2/windows_x86
+mkdir -p lua5.2/windows_x86_64
+mkdir -p lua5.2/ubuntu_1404_x86
+mkdir -p lua5.2/ubuntu_1404_x86_64
+mkdir -p lua5.2/ubuntu_1604_x86
+mkdir -p lua5.2/ubuntu_1604_x86_64
+mkdir -p lua5.2/ubuntu_1704_x86
+mkdir -p lua5.2/ubuntu_1704_x86_64
+mkdir -p lua5.3/windows_x86
+mkdir -p lua5.3/windows_x86_64
+mkdir -p lua5.3/ubuntu_1404_x86
+mkdir -p lua5.3/ubuntu_1404_x86_64
+mkdir -p lua5.3/ubuntu_1604_x86
+mkdir -p lua5.3/ubuntu_1604_x86_64
+mkdir -p lua5.3/ubuntu_1704_x86
+mkdir -p lua5.3/ubuntu_1704_x86_64
+
+
+
+tar --extract --directory lua5.1/windows_x86 --file build_windows_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86 --file build_windows_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86 --file build_windows_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/windows_x86_64 --file build_windows_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86_64 --file build_windows_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86_64 --file build_windows_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.3.tar.gz --gzip
+
+popd
+
+
+pushd build/com.github.brimworks-lua5.1-lua-zlib
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua-zlib/installer/lua5.1
+make
+make package
+popd
+
+pushd build/com.github.brimworks-lua5.2-lua-zlib
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua-zlib/installer/lua5.2
+make
+make package
+popd
+
+pushd build/com.github.brimworks-lua5.3-lua-zlib
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua-zlib/installer/lua5.3
+make
+make package
+popd
